cdfe3a58e2f28f569bb0a7281ff9fbe5b411f711
Ivan Vecerina <IvanR.vecerina@gmail.com>
2021-11-11 16:20:45 +0100

    horaire disgustingly done

--- src/Lecon.java
+++ src/Lecon.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Lecon {
     private int jourSemaine;
     private int periodeDebut;
@@ -26,8 +28,58 @@ public class Lecon {
         this.professeur.getListeLecon().add(this);
     }
 
-    static public String horaire(Object args){
-        return "";
+    static public String horaire(Lecon[] lecons){
+        String[][] tab = new String[5][11];
+        String tmp;
+
+        for (Lecon lecon : lecons) {
+            tmp = lecon.professeur == null ? "   " : lecon.professeur.abreviation();
+
+            tab[lecon.jourSemaine][lecon.periodeDebut] = lecon.matiere + "   " + lecon.salle + " " + tmp;
+
+            for (int i = 1; i < lecon.duree; i++){
+                tab[lecon.jourSemaine][lecon.periodeDebut+i] = "";
+            }
+        }
+
+
+        String   days     = "     | Lun         | Mar         | Mer         | Jeu         | Ven         |\n"+
+                            "     |-------------|-------------|-------------|-------------|-------------|\n";
+        String[] periods = {" 8:30|",
+                            " 9:15|",
+                            "10:25|",
+                            "11:15|",
+                            "12:00|",
+                            "13:15|",
+                            "14:00|",
+                            "14:55|",
+                            "15:45|",
+                            "16:35|",
+                            "17:20|"};
+
+        String periodBorder = "-------------";
+        String emptyCell    = "             ";
+
+        String toPrint = days;
+
+        for (int i = 0; i < tab[0].length; i++){
+            toPrint += periods[i];
+            for (int j = 0; j < tab.length; j++){
+                tmp = tab[j][i] == null || tab[j][i].equals("") ? emptyCell : tab[j][i];
+                toPrint += tmp + "|";
+            }
+            toPrint += "\n     |";
+
+            for (int j = 0; j < tab.length; j++){
+                tmp = tab[j][i+1 < tab[0].length ? i+1: 0] == "" ? emptyCell : periodBorder;
+                toPrint += tmp + "|";
+            }
+            toPrint += "\n";
+
+        }
+
+
+        return toPrint;
     }
 
 
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,20 @@
 public class Test {
+
+
+    public static void main(String args[]){
+        Lecon[] lecons = new Lecon[4];
+        Lecon l1 = new Lecon(1, 1, 2, "POO", "H03");
+        Lecon l2 = new Lecon(2, 5, 3, "SYE", "H03");
+        Lecon l3 = new Lecon(3, 7, 1, "POO", "H03");
+        Lecon l4 = new Lecon(3, 8, 2, "POO", "H03");
+
+        lecons[0] = l1;
+        lecons[1] = l2;
+        lecons[2] = l3;
+        lecons[3] = l4;
+
+        System.out.println(Lecon.horaire(lecons));
+
+    }
+
 }
