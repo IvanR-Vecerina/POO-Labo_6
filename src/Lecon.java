83c3333cf4b4873a4f9f46a3a8619552dea529db
MasterZeus97 <thibault.seem@heig-vd.ch>
2021-11-11 15:00:44 +0100

    Ajout constructeurs Lecon et methode pour get la liste des lecon d'un prof

--- src/Lecon.java
+++ src/Lecon.java
@@ -6,6 +6,26 @@ public class Lecon {
     String salle;
     Professeur professeur;
 
+    public Lecon(int jourSemaine, int periodeDebut, int duree, String matiere, String salle) {
+        this.jourSemaine = jourSemaine;
+        this.periodeDebut = periodeDebut;
+        this.duree = duree;
+        this.matiere = matiere;
+        this.salle = salle;
+    }
+
+    public Lecon(int jourSemaine, int periodeDebut, int duree, String matiere, String salle, Professeur professeur){
+        /*this.jourSemaine = jourSemaine;
+        this.periodeDebut = periodeDebut;
+        this.duree = duree;
+        this.matiere = matiere;
+        this.salle = salle;*/
+        this(jourSemaine, periodeDebut, duree, matiere, salle);
+        this.professeur = professeur;
+
+        this.professeur.getListeLecon().add(this);
+    }
+
     static public String horaire(Object args){
         return "";
     }
--- src/Professeur.java
+++ src/Professeur.java
@@ -22,4 +22,8 @@ public class Professeur extends Personne {
     public String toString(){
         return "";
     }
+
+    public ArrayList<Lecon> getListeLecon(){
+        return listLecon;
+    }
 }
